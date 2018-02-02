6b773b251ecb8ccd367b9819146b8f5209f61c55
neifirst <e.mcculloch.arch@gmail.com>
2018-02-02 15:15:03 +0000

    sorted playlist

--- specs/bar_spec.rb
+++ specs/bar_spec.rb
@@ -3,15 +3,46 @@ require("minitest/rg")
 
 require_relative("../bar.rb")
 require_relative("../room.rb")
+require_relative("../guest.rb")
+require_relative("../song.rb")
 
 
 class TestBar < MiniTest::Test
 
   def setup
 
-    @room1 = Room.new([], 20, [])
-    @room2 = Room.new([], 40, [])
-    @room3 = Room.new([], 10, [])
+    # sexy = Song.new("Sexy And I Know It", "LMFAO")
+    #
+    # rasputin = Song.new("Rasputin", "Boney M")
+    #
+    # gay_bar = Song.new("Gay Bar", "Electric Six")
+    #
+    # baby = Song.new("Baby Got Back", "Sir Mix-A-Lot")
+    #
+    # dont = Song.new("Don't Stop Me Now", "Queen")
+    #
+    # bamba = Song.new("La Bamba", "Richie Valens")
+    #
+    # mplaylist = {
+    #
+    #   playlist1: [
+    #     sexy, rasputin
+    #   ],
+    #
+    #   playlist2: [
+    #     baby, dont
+    #   ],
+    #
+    #   playlist3: [
+    #     bamba, gay_bar
+    #   ]
+    # }
+    #
+    @room1 = Room.new([], 20, mplaylist[:playlist1])
+
+    @room2 = Room.new([], 40, mplaylist[:playlist2])
+
+    @room3 = Room.new([], 10, mplaylist[:playlist3])
 
     rooms = [@room1, @room2, @room3]
 
@@ -29,6 +60,15 @@ class TestBar < MiniTest::Test
   def test_get_rooms
 
     assert_equal([@room1, @room2, @room3], @bar.rooms())
+
+  end
+
+  def test_add_guest
+
+    guest_name = "Jeff Bridges"
+    room_number = 1
+
+    assert_equal("Jeff Bridges", @room1.guests())
 
   end
 
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -2,13 +2,36 @@ require("minitest/autorun")
 require("minitest/rg")
 
 require_relative("../room.rb")
+require_relative("../song.rb")
 
 
 class TestRoom < MiniTest::Test
 
   def setup
 
-    @room1 = Room.new([], 20, [])
+    @song1 = Song.new("Sexy And I Know It", "LMFAO")
+
+    @song2 = Song.new("Rasputin", "Boney M")
+
+    @song3 = Song.new("Gay Bar", "Electric Six")
+
+    @song4 = Song.new("Baby Got Back", "Sir Mix-A-Lot")
+
+    @song5 = Song.new("Don't Stop Me Now", "Queen")
+
+    @song6 = Song.new("La Bamba", "Richie Valens")
+
+    @playlist = [@song1, @song2, @song3, @song4, @song5, @song6]
+
+
+
+
+    @room1 = Room.new([], 20, @playlist)
+
+    @room2 = Room.new([], 40, @playlist)
+
+    @room3 = Room.new([], 10, @playlist)
+
 
   end
 
@@ -28,9 +51,14 @@ class TestRoom < MiniTest::Test
 
   def test_get_room_songs
 
-    assert_equal([], @room1.songs)
+    assert_equal(@playlist, @room1.songs)
 
   end
+
+
+
+
+
 
 
 
