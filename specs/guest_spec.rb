def0af5a2874f1c3fa8269221fb379f7b26d18ef
neifirst <e.mcculloch.arch@gmail.com>
2018-02-04 12:38:13 +0000

    added can pay fee to bar test

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -2,6 +2,8 @@ require("minitest/autorun")
 require("minitest/rg")
 
 require_relative("../guest.rb")
+require_relative("../bar.rb")
+require_relative("../room.rb")
 
 
 class TestGuest < MiniTest::Test
@@ -10,7 +12,19 @@ class TestGuest < MiniTest::Test
 
     @guest1 = Guest.new("Jeff Bridges", 50)
 
+    @room1 = Room.new(2)
+
+    @room2 = Room.new(40)
+
+    @room3 = Room.new(20)
+
+    rooms = [@room1, @room2, @room3]
+
+    @bar = Bar.new("Jeff's Happy Karaoke Bar", rooms)
+
   end
+
+
 
   def test_get_guest_name
 
@@ -31,6 +45,18 @@ class TestGuest < MiniTest::Test
     @guest1.pay_fee(amount)
 
     assert_equal(45, @guest1.cash)
+
+  end
+
+  def test_pay_fee_to_bar
+
+    amount = 5
+
+    @guest1.pay_fee(amount)
+    @bar.add_to_till(amount)
+
+    assert_equal(45, @guest1.cash())
+    assert_equal(5, @bar.till())
 
   end
 
